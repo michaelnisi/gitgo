b9416c39377231bf3d8a5ea6f9d3717c2388b0f4
Michael Nisi <michael.nisi@gmail.com>
2013-12-10 18:25:41 +0100

    Node v0.11.x

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var Readable = require('stream').Readable
   , spawn = require('child_process').spawn
 
 module.exports = function (path, opts) {
-  var ps = spawn('git', opts, { cwd: path })
+  var ps = spawn('git', opts, psopts(path))
     , stdout = ps.stdout
     , stderr = ps.stderr
     , stream = new Readable()
@@ -14,31 +14,34 @@ module.exports = function (path, opts) {
     stream.push(null)
     stream.emit('close')
   })
-
   ps.on('exit', function () {
     ps.kill()
   })
-
-  ps.on('error', function (err) {
-    stream.emit(err)
+  ps.on('error', function (er) {
+    stream.emit(er)
   })
 
   stdout.on('readable', function () {
     stream.read(0)
   })
-
-  stdout.on('error', function (err) {
-    stream.emit(err)
+  stdout.on('error', function (er) {
+    stream.emit(er)
   })
 
-  stderr.on('data', function (chunk) {
-    stream.emit('error', new Error(chunk))
+  stderr.on('data', function (er) {
+    stream.emit('error', new Error(er))
   })
 
   stream._read = function (size) {
     var chunk = stdout.read(size)
     stream.push(chunk === null ? '' : chunk)
   }
-
   return stream
+}
+
+function psopts (cwd) {
+  return {
+    cwd:cwd
+  , env:process.env
+  }
 }
--- test/commands.js
+++ test/commands.js
@@ -6,6 +6,27 @@ var test = require('tap').test
   , spawn = require('child_process').spawn
   , gitgo = require('../')
   , es = require('event-stream')
+  , StringDecoder = require('string_decoder').StringDecoder
+
+var decoder = new StringDecoder()
+function dec(buf) {
+  return decoder.write(buf)
+}
+
+function tt(opts, t) {
+  var git = gitgo(dir, opts)
+    , actual = []
+  git.on('end', function () {
+    t.is(actual.length, 1)
+    t.end()
+  })
+  git.on('readable', function () {
+    var chunk;
+    while (null !== (chunk = git.read())) {
+      actual.push(chunk)
+    }
+  })
+}
 
 test('setup', function (t) {
   fs.mkdirSync(dir, 0700)
@@ -14,70 +35,42 @@ test('setup', function (t) {
 
 test('not a git repository', function (t) {
   var git = gitgo(dir, ['pull'])
-
+    , error = null
   git.on('end', function () {
     t.ok(error, 'should have caught error')
     t.end()
   })
-
-  var error
   git.on('error', function (er) {
     error = er
   })
-
   git.on('readable', function () {
-    t.ok(git.read(), 'should not be null')
+    while (null !== git.read()) {}
   })
 })
 
 test('git init', function (t) {
-  var git = gitgo(dir, ['init'])
-
-  git.on('end', function () {
-    t.ok(true, 'should be ok')
-    t.end()
-  })
-
-  git.on('readable', function () {
-    t.ok(git.read(), 'should not be null')
-  })
+  tt(['init'], t)
 })
 
-test('git add', function (t) {
+test('git add (flowing)', function (t) {
   var filename = join(dir, 'hello.js')
   fs.writeFile(filename, 'console.log("Hello World!")', function (err) {
     var git = gitgo(dir, ['add', '.'])
-
     git.on('end', function () {
       t.ok(true, 'should be ok')
       t.end()
     })
-
+    git.resume()
     t.equal(null, git.read(), 'should be null')
   })
 })
 
 test('commit (non-flowing)', function (t) {
-  var git = gitgo(dir, ['commit', '-m', 'Add hello file'])
-
-  var found = []
-  git.on('end', function () {
-    t.ok(found.length, 'should have read something')
-    t.end()
-  })
-
-  var count = 0
-  git.on('readable', function () {
-    var chunk
-    while (null !== (chunk = git.read())) {
-      found.push(chunk)
-    }
-  })
+  tt(['commit', '-m', 'Add hello file'], t)
 })
 
 test('log (flowing)', function (t) {
   var git = gitgo(dir, ['log'])
-
   git.pipe(es.writeArray(function (err, lines) {
     t.ok(lines.length, 'should have read something')
     t.end()
