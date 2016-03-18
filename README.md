b5d6944f951ac07b9b4504cadbe2a2d0895a27d1
Michael Nisi <michael.nisi@gmail.com>
2016-03-18 17:46:18 +0100

    Comply with standard

--- README.md
+++ README.md
@@ -2,16 +2,17 @@
 
 The `gitgo` [Node](http://nodejs.org/) module makes it slightly easier to execute [Git](http://git-scm.com/) commands from Node. It's a one-off function that spawns a child process to run the Git command, returning a readable stream to expose feedback from Git.
 
-[![Build Status](https://secure.travis-ci.org/michaelnisi/gitgo.png?branch=master)](https://travis-ci.org/michaelnisi/gitgo) [![David DM](https://david-dm.org/michaelnisi/gitgo.png)](http://david-dm.org/michaelnisi/gitgo)
+[![Build Status](https://secure.travis-ci.org/michaelnisi/gitgo.png?branch=master)](https://travis-ci.org/michaelnisi/gitgo)
 
 ## Usage
 
 ### Cloning a repo
+
 ```js
 var gitgo = require('gitgo')
-  , fs = require('fs')
-  , repo = 'git://github.com/michaelnisi/gitgo.git'
-  , path = '/tmp/gitgo-' + Math.floor(Math.random() * (1<<24))
+var fs = require('fs')
+var repo = 'git://github.com/michaelnisi/gitgo.git'
+var path = '/tmp/gitgo-' + Math.floor(Math.random() * (1 << 24))
 
 fs.mkdirSync(path)
 gitgo(path, ['clone', repo]).pipe(process.stdout)
--- index.js
+++ index.js
@@ -1,56 +1,57 @@
-
 // gitgo - the stupid git wrapper
 
 module.exports = GitGo
 
+var StringDecoder = require('string_decoder').StringDecoder
+var child_process = require('child_process')
+var stream = require('readable-stream')
+var util = require('util')
+
 function psopts (cwd) {
   return {
-    cwd:cwd
-  , env:process.env
+    cwd: cwd,
+    env: process.env
   }
 }
 
-var child_process = require('child_process')
+function spawn (s) {
+  var dir = s.dir
+  var opts = s.opts
+  var ps = child_process.spawn('git', opts, psopts(dir))
 
-function spawn (stream) {
-  var dir = stream.dir
-    , opts = stream.opts
-    , ps = child_process.spawn('git', opts, psopts(dir))
   ps.on('close', function () {
-    stream.push(null)
-    stream.emit('close')
+    s.push(null)
+    s.emit('close')
   })
   function error (er) {
-    stream.emit('error', er)
+    s.emit('error', er)
   }
   ps.on('error', error)
   ps.stdout.on('error', error)
-  ps.stdout.pipe(stream)
-  ps.stderr.pipe(stream) // reading non-error stuff here too
+  ps.stdout.pipe(s)
+  ps.stderr.pipe(s) // reading non-error stuff here too
   return ps
 }
-
-var util = require('util')
-  , stream = require('stream')
 
 function GitGo (dir, opts) {
   if (!(this instanceof GitGo)) return new GitGo(dir, opts)
   stream.Transform.call(this)
   this.dir = dir
   this.opts = opts
+  this.decoder = new StringDecoder(opts.encoding)
   spawn(this)
 }
+
 util.inherits(GitGo, stream.Transform)
 
-var StringDecoder = require('string_decoder').StringDecoder
-
-function error (chunk) {
-  var str = new StringDecoder().write(chunk)
+function error (chunk, decoder) {
+  decoder = decoder || new StringDecoder()
+  var str = decoder.write(chunk)
   return str.split(' ')[0] === 'fatal:' ? new Error(str) : null
 }
 
 GitGo.prototype._transform = function (chunk, enc, cb) {
-  var er = error(chunk)
+  var er = error(chunk, this.decoder)
   if (er) this.emit('error', er)
   this.push(chunk)
   cb()
